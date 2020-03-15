3e9dbde14a7c4e2bca0d6ec71dd50f110ab28f45
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-03-15 21:42:06 +0800

    Styled Welcome Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_081158) do
+ActiveRecord::Schema.define(version: 2020_03_15_101132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_03_12_081158) do
     t.bigint "flat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["flat_id"], name: "index_contacts_on_flat_id"
   end
 
